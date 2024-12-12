8e31fe576e152994bff121752f4c5a0747cb31e4
Azim19062006 <mamatbaevazim06@gmail.com>
2024-12-12 12:49:38 +0600

    COmpleted Input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   return (
     <div>
-      {/* <Button variant="bordered">Confirm</Button> */}
+      <h1>Button Component</h1>
       <Button size="sm">Confirm</Button>
-      {/* <Button>Cancel</Button> */}
       <Button size="md">Confirm</Button>
       <Button size="lg">Confirm</Button>
       <br />
       <Button variant="bordered" size="sm">Confirm</Button>
       <Button variant="bordered" size="md">Confirm</Button>
       <Button variant="bordered" size="lg">Confirm</Button>
+
+      <h1>Input Component</h1>
+
+      <Input name="Name" value="Ivan Ivanov" label="Name"/>
+      <Input name="Focus" value="Focus" label="Name"/>
+      <Input name="Danger" value="Danger" danger label="Name"/>
     </div>
   );
 }
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const Input = ({
+  type = 'text',
+  placeholder,
+  value,
+  onChange,
+  disabled = false,
+  label,
+  danger = false,
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const inputStyles = `
+    input-component
+    ${danger ? 'input-component--danger' : 'input-component--normal'}
+    ${isFocused ? 'input-component--focused' : ''}
+  `;
+
+  return (
+    <div className="input-wrapper">
+      {label && (
+        <label htmlFor={label.toLowerCase()} className="input-label">
+          {label}
+        </label>
+      )}
+      <input
+        id={label?.toLowerCase()}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        className={inputStyles}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+    </div>
+  );
+};
+
+export default Input;
